c81ecdbb68bf79f82d6daef5cc3f52123b4e1315
unknown <syedsabeera391@gmail.com>
2025-02-23 23:33:21 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:porfolio/PortFolio.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor:
+            Colors.black, // Set background color for scaffold
+      ),
+      debugShowCheckedModeBanner: false,
+      title: 'PortFolio',
+      home: PortFolio(),
+    );
+  }
+}
